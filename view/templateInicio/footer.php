06feef85704e5ec40c180259c70d6c4857f52e0b
Jhonny-Morocho <37280267+Jhonny-Morocho@users.noreply.github.com>
2020-10-09 15:12:31 -0500

    Estilos al footer

--- view/templateInicio/footer.php
+++ view/templateInicio/footer.php
@@ -1,104 +1,165 @@
 	<!-- =============================== FOOTER ===================-->
 	<!-- =============================== FOOTER ===================-->
-	<div class=" ">
-			<!-- Footer -->
-			<footer class="page-footer font-small teal pt-4 black ">
-
-				<!-- Footer Text -->
-				<div class="container-fluid text-center text-md-left">
-			
-				<!-- Grid row -->
-				<div class="row">
-			
+<!-- Footer -->
+<footer class="page-footer font-small black">
+		<div style="background-color:red;">
+			<div class="container">
+				<!-- Grid row-->
+				<div class="row py-4 d-flex align-items-center">
 					<!-- Grid column -->
-					<div class="col-md-6 mt-md-0 mt-3">
-			
-					<!-- Content -->
-					<h5 class="text-uppercase font-weight-bold">Exclusive Remixes & Edits</h5>
-					<p>Djs and producers from all over the world creating exclusive and professional versions for djs</p>
-			
+					<div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+						<h6 class="mb-0">EXCLUSIVE REMIXES & EDITS FOR DJS</h6>
 					</div>
 					<!-- Grid column -->
-			
-					<hr class="clearfix w-100 d-md-none pb-3">
-			
+
 					<!-- Grid column -->
-					<div class="col-md-6 mb-md-0 mb-3">
-			
-					<!-- Content -->
-					<h5 class="text-uppercase font-weight-bold">The best of the record pool of djs</h5>
-					<p>The complete Latin music platform, acquire exclusive versions</p>
-			
+					<div class="col-md-6 col-lg-7 text-center text-md-right">
+
+						<!-- Facebook -->
+						<a class="fb-ic" href="https://www.facebook.com/Proeditsclub-702404853464740/" target="alt">
+							<i class="fab fa-facebook-f white-text mr-4"> </i>
+						</a>
+						<!-- wassap -->
+						<a class="tw-ic" target="alt"  href="https://api.whatsapp.com/send?phone=+593998202201&text=Yo%27%20Tengo%20una%20consulta%20">
+							<i class="fab fa-whatsapp mr-4 white-text"></i>
+						</a>
+						<!-- Google +-->
+						<!-- <a class="gplus-ic">
+						<i class="fab fa-google-plus-g white-text mr-4"> </i>
+						</a> -->
+						<!--Linkedin -->
+						<!-- <a class="li-ic">
+						<i class="fab fa-linkedin-in white-text mr-4"> </i>
+						</a> -->
+						<!--Instagram-->
+						<a class="ins-ic" href="https://www.instagram.com/proeditsclub/" target="alt">
+							<i class="fab fa-instagram white-text"> </i>
+						</a>
+
 					</div>
 					<!-- Grid column -->
-			
 				</div>
-				<!-- Grid row -->
-			
-				</div>
-				<!-- Footer Text -->
-			
-				<!-- Copyright -->
-				<div class="footer-copyright text-center py-3">© 2020 Copyright:
-					<a href="https://wwww.proeditsclub.com"> www.proeditsclub.com</a>
-					
-				</div>
-				<!-- Copyright -->
-			
-			</footer>
-			<!-- Footer -->
-	</div>
-
-	<div class="">
-		<div class="row">
-			<div class="col-lg-12">
-	
+				<!-- Grid row-->
 			</div>
 		</div>
-	</div>
-	<!-- Footer -->
 
+		<!-- Footer Links -->
+		<div class="container text-center text-md-left mt-5 ">
+			<!-- Grid row -->
+			<div class="row mt-3">
+				<!-- Grid column -->
+				<div class="col-md-2 col-lg-3 col-xl-3 mx-auto mb-3">
+					<!-- Content -->
+					<h6 class="text-uppercase font-weight-bold">PROEDITSCLUB.COM</h6>
+					<hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+					<p>The complete Latin music platform, acquire exclusive versions</p>
+				</div>
+				<!-- Grid column -->
+
+				<!-- Grid column -->
+				<div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+					<!-- Links -->
+					<h6 class="text-uppercase font-weight-bold">Products</h6>
+					<hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+					<p>
+						<a href="../../membresia.php">Membresias</a>
+					</p>
+					<p>
+						<a href="https://api.whatsapp.com/send?phone=+593998202201&text=Yo%27Deseo%20recargar%20mi%20monedero">Monedero</a>
+					</p>
+					 <p>
+						<a href="https://api.whatsapp.com/send?phone=+593998202201&text=Deseo%20ser%20parte%20de%20su%20equipo">Remixer Application</a>
+					</p>
+					<p>
+						<a href="https://api.whatsapp.com/send?phone=+593998202201&text=Quiero%20saber%20de%20su%20servicio%20%20Ing%20software%20development">Ing software development</a>
+					</p> 
+				</div>
+				<!-- Grid column -->
+
+				<!-- Grid column -->
+				<div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+					<!-- Links -->
+					<h6 class="text-uppercase font-weight-bold">Useful links</h6>
+					<hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+					<p>
+						<a href="../../login.php">Your Account</a>
+					</p>
+					<p>
+						<a href="../../registro.php">Create Account</a>
+					</p>
+	
+					<p>
+						<a href="https://api.whatsapp.com/send?phone=+593998202201&text=Yo%27%20Tengo%20una%20consulta%20">Help</a>
+					</p>
+				</div>
+				<!-- Grid column -->
+
+				<!-- Grid column -->
+				<div class="col-md-4 col-lg-4 col-xl-3 mx-auto mb-md-0 mb-4">
+					<!-- Links -->
+					<h6 class="text-uppercase font-weight-bold">Contact</h6>
+					<hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+					<p>
+						<i class="fas fa-home mr-2"></i> Loja, Loja 0034, EC</p>
+					<p>
+						<i class="fas fa-envelope mr-2"></i>jhonnymichaeldj2011@hotmail.com</p>
+					<p>
+						<i class="fab fa-whatsapp mr-2"></i>+ 593 998202201</p>
+					<p>
+				</div>
+				<!-- Grid column -->
+			</div>
+  			<!-- Grid row -->
+		</div>
+		
+		<!-- Copyright -->
+		<div class="footer-copyright text-center py-3 ">© 2020 Copyright:
+			<a href="https://mdbootstrap.com/"> proeditsclub.com</a>
+		</div>
+		<!-- Copyright -->
+	<!-- Footer Links -->
+</footer>
+<!-- Footer -->
 
 	<!-- jQuery -->
-	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/jquery.min.js?v=2.0.0"></script>
 
 	<!-- Bootstrap tooltips -->
-	<script type="text/javascript" src="js/popper.min.js"></script>
+	<script type="text/javascript" src="js/popper.min.js?v=2.0.0"></script>
 	<!-- Bootstrap core JavaScript -->
-	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js?v=2.0.0"></script>
 	<!-- MDB core JavaScript -->
-	<script type="text/javascript" src="js/mdb.min.js"></script>
+	<script type="text/javascript" src="js/mdb.min.js?v=2.0.0"></script>
 	<!-- MDB Component Initialization -->
-	<script type="text/javascript" src="../js/appIndex.js"></script>
+	<script type="text/javascript" src="../js/appIndex.js?v=2.0.0"></script>
 	<!-- Encoder.js -->
-	<script type="text/javascript" src="../js/encoder.js"></script>
+	<script type="text/javascript" src="../js/encoder.js?v=2.0.0"></script>
 	<!-- sprintf-js -->
-	<script type="text/javascript" src="../js/sprintf.min.js"></script>
+	<script type="text/javascript" src="../js/sprintf.min.js?v=2.0.0"></script>
   <!-- Your custom scripts (optional) -->
 
   
     <!--========================ANIMACION DE ESPERA==================================================== -->
-    <script type="text/javascript" src="../../controler/js/animacionEspera.js?v=1.0.0"></script> 
+    <script type="text/javascript" src="../../controler/js/animacionEspera.js?v=2.0.0"></script> 
     <!-- ======================AJAX================================== -->
-    <script type="text/javascript" src="../../controler/js/ajaxProveedor.js?v=1.0.0"></script>
-	<script type="text/javascript" src="../../controler/js/ajaxCliente.js?v=1.0.0"></script>
-	<script type="text/javascript" src="../../controler/js/pagarMembresia.js?v=1.0.0"></script>
+    <script type="text/javascript" src="../../controler/js/ajaxProveedor.js?v=2.0.1"></script>
+	<script type="text/javascript" src="../../controler/js/ajaxCliente.js?v=2.0.1"></script>
+	<script type="text/javascript" src="../../controler/js/pagarMembresia.js?v=2.0.0"></script>
 	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 	
     <script src="https://unpkg.com/moment"></script>
-    <script type="text/javascript" src="../../controler/js/carritoCompras.js?v=1.0.1"></script>
-    <script type="text/javascript" src="../../controler/js/ajaxPagar.js?v=1.0.1"></script>
+    <script type="text/javascript" src="../../controler/js/carritoCompras.js?v=2.0.1"></script>
+    <script type="text/javascript" src="../../controler/js/ajaxPagar.js?v=2.0.0"></script>
 	<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
 	<!-- ===================== REDES SOCIALES ======================== -->
-	  <script src="../../socialNav/social.js"></script>
-
+	  <script src="../../socialNav/social.js?v=2.0.0"></script>
 	<!-- ======================= REPRODUCTOS DE AUDIO  WAVE ======================= -->
-	<!-- <script src="https://unpkg.com/wavesurfer.js"></script> -->
-	<script src="../../waveSurfer/wavesurfer.min.js"></script>
+	<script src="../../waveSurfer/wavesurfer.min.js?v=2.0.0"></script>
 
- 	<script src="../../waveSurfer/app.js"></script>
-	  
+ 	<script src="../../waveSurfer/app.js?v=2.0.3"></script>
 
+	<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/wavesurfer.js/1.0.52/wavesurfer.min.js?v=1.0.01"></script> -->
 
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-136516597-1"></script>
@@ -111,6 +172,64 @@
 	</script>
 
 
+
+	<script>
+	$(document).ready(function() {
+	$(".jm-loadingpage").fadeOut("slow");;
+	});
+	</script>
+
+
+	<!-- =========== HALLOWIN VAMPIROS========== -->
+	<SCRipt>
+	/*! Halloween bats - Luca Munich - https://devunit.org */
+	;(function () {
+	var r=Math.random,n=0,d=document,w=window,
+		i=d.createElement('img'),
+		z=d.createElement('div'),
+		zs=z.style,
+		a=w.innerWidth*r(),b=w.innerHeight*r();
+	zs.position="fixed";
+	zs.left=0;
+	zs.top=0;
+	zs.opacity=0;
+	z.appendChild(i);
+	i.src='../../img/fant.gif';
+	d.body.appendChild(z);
+	function R(o,m){return Math.max(Math.min(o+(r()-.5)*400,m-50),50)}
+	function A(){
+		var x=R(a,w.innerWidth),y=R(b,w.innerHeight),
+			d=5*Math.sqrt((a-x)*(a-x)+(b-y)*(b-y));
+		zs.opacity=n;n=1;
+		zs.transition=zs.webkitTransition=d/1e3+'s linear';
+		zs.transform=zs.webkitTransform='translate('+x+'px,'+y+'px)';
+		i.style.transform=i.style.webkitTransform=(a>x)?'':'scaleX(-1)';
+		a=x;b=y;
+		setTimeout(A,d);
+	};setTimeout(A,r()*3e3);
+	})();
+	</SCRipt>
+
+
+	<!-- Facebook Pixel Code -->
+	<script>
+	!function(f,b,e,v,n,t,s)
+	{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+	if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+	n.queue=[];t=b.createElement(e);t.async=!0;
+	t.src=v;s=b.getElementsByTagName(e)[0];
+	s.parentNode.insertBefore(t,s)}(window, document,'script',
+	'https://connect.facebook.net/en_US/fbevents.js');
+	fbq('init', '619383111865354');
+	fbq('track', 'PageView');
+	</script>
+	<noscript><img height="1" width="1" style="display:none"
+	src="https://www.facebook.com/tr?id=619383111865354&ev=PageView&noscript=1"
+	/></noscript>
+	<!-- End Facebook Pixel Code -->
 </body>
 
 </html>
+
+
